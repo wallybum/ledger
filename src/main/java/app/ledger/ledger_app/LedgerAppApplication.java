c67aa613e91cb4e9f653667e2c59be4612a7d557
elvis <elvis@elvisui-Macmini.local>
2025-05-26 11:19:25 +0900

    최초 커밋

---
+++ src/main/java/app/ledger/ledger_app/LedgerAppApplication.java
@@ -0,0 +1,13 @@
+package app.ledger.ledger_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/ledger/ledger_app/LedgerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ledger.ledger_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
